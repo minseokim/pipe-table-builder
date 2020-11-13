4801521fbebedd6bf2d16189b5655e1c80b55336
minseokim <minseoalexkim@gmail.com>
2020-11-13 11:43:34 +0900

    Move all state logic to tableBuilder component

--- project/src/components/TableBuilder.js
+++ project/src/components/TableBuilder.js
@@ -1,13 +1,82 @@
-import React, { Fragment } from 'react';
-
+/* eslint-disable no-param-reassign */
+import produce from 'immer';
+import React, { useEffect, useState } from 'react';
+import customerDataSource from '../dataSource/customerDataSource';
 import Header from './Header';
 import TableDashboard from './TableDashboard';
 
+// TODO : Refactor using Context
+//        Disable operatorAmount when 'No Operator' is selected in filter
+//        Add color to column when selected
+//        Validate JSON Config(Nice-to-have)
+//        Styling
+//        Unit Tests()
+
 const TableBuilder = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [customerDataList, setCustomerDataList] = useState(customerDataSource);
+
+  const [columnSettings, setColumnSettings] = useState(null);
+
+  useEffect(() => {
+    if (customerDataList && customerDataList.length) {
+      const firstCustomerData = customerDataList[0];
+
+      const initialColumnSettings = {};
+
+      Object.keys(firstCustomerData).forEach((key) => {
+        const value = firstCustomerData[key];
+        initialColumnSettings[key] = {
+          id: key,
+          name: key,
+          isFilterable: !Number.isNaN(Number(value)),
+          filterOperator: null,
+          filterAmount: null,
+          shouldDisplay: false,
+        };
+      });
+
+      setColumnSettings(initialColumnSettings);
+    }
+  }, [customerDataList]);
+
+  const handleColumnToggle = (columnName, isSelected) => {
+    setColumnSettings(
+      produce(
+        (columnSettings,
+        (draftColumnSettings) => {
+          draftColumnSettings[columnName].shouldDisplay = isSelected;
+        })
+      )
+    );
+  };
+
+  const handleApplyFilter = (columnName, filterOperator, filterAmount) => {
+    console.log('columnName', columnName, filterOperator, filterAmount);
+    setColumnSettings(
+      produce(
+        (columnSettings,
+        (draftColumnSettings) => {
+          draftColumnSettings[columnName].filterOperator = filterOperator;
+          draftColumnSettings[columnName].filterAmount = filterAmount;
+        })
+      )
+    );
+  };
+
   return (
     <>
-      <Header />
-      <TableDashboard />
+      {columnSettings ? (
+        <>
+          <Header />
+          <TableDashboard
+            columnSettings={columnSettings}
+            customerDataList={customerDataList}
+            onColumnToggle={handleColumnToggle}
+            onApplyFilter={handleApplyFilter}
+          />
+        </>
+      ) : null}
     </>
   );
 };
--- project/src/components/TableDashboard.js
+++ project/src/components/TableDashboard.js
@@ -1,91 +1,58 @@
 /* eslint-disable no-param-reassign */
 import { majorScale, Pane } from 'evergreen-ui';
-import { produce } from 'immer';
-import React, { useEffect, useState } from 'react';
-import customerDataSource from '../dataSource/customerDataSource';
+import PropTypes from 'prop-types';
+import React from 'react';
 import EditPanel from './EditPanel';
 import ViewTable from './ViewTable';
 
-// TODO : Refactor using Context
-//        Disable operatorAmount when 'No Operator' is selected in filter
-//        Add color to column when selected
-//        Validate JSON Config(Nice-to-have)
-//        Styling
-//        Unit Tests()
-
-const TableDashboard = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [customerDataList, setCustomerDataList] = useState(customerDataSource);
-
-  const [columnSettings, setColumnSettings] = useState(null);
-
-  useEffect(() => {
-    if (customerDataList && customerDataList.length) {
-      const firstCustomerData = customerDataList[0];
-
-      const initialColumnSettings = {};
-
-      Object.keys(firstCustomerData).forEach((key) => {
-        const value = firstCustomerData[key];
-        initialColumnSettings[key] = {
-          id: key,
-          name: key,
-          isFilterable: !Number.isNaN(Number(value)),
-          filterOperator: null,
-          filterAmount: null,
-          shouldDisplay: false,
-        };
-      });
-
-      setColumnSettings(initialColumnSettings);
-    }
-  }, [customerDataList]);
-
-  const handleColumnToggle = (columnName, isSelected) => {
-    setColumnSettings(
-      produce(
-        (columnSettings,
-        (draftColumnSettings) => {
-          draftColumnSettings[columnName].shouldDisplay = isSelected;
-        })
-      )
-    );
-  };
-
-  const handleApplyFilter = (columnName, filterOperator, filterAmount) => {
-    console.log('columnName', columnName, filterOperator, filterAmount);
-    setColumnSettings(
-      produce(
-        (columnSettings,
-        (draftColumnSettings) => {
-          draftColumnSettings[columnName].filterOperator = filterOperator;
-          draftColumnSettings[columnName].filterAmount = filterAmount;
-        })
-      )
-    );
-  };
-
+const TableDashboard = ({
+  columnSettings,
+  customerDataList,
+  onColumnToggle,
+  onApplyFilter,
+}) => {
   return (
     <Pane
       height={`calc(100vh - ${majorScale(7)}px)`}
       display="flex"
       background="tint2"
     >
-      {columnSettings ? (
-        <>
-          <EditPanel
-            columnSettings={columnSettings}
-            onColumnToggle={handleColumnToggle}
-            onApplyFilter={handleApplyFilter}
-          />
-          <ViewTable
-            columnSettings={columnSettings}
-            customerDataList={customerDataList}
-          />
-        </>
-      ) : null}
+      <EditPanel
+        columnSettings={columnSettings}
+        onColumnToggle={onColumnToggle}
+        onApplyFilter={onApplyFilter}
+      />
+      <ViewTable
+        columnSettings={columnSettings}
+        customerDataList={customerDataList}
+      />
     </Pane>
   );
+};
+
+TableDashboard.propTypes = {
+  customerDataList: PropTypes.arrayOf(
+    PropTypes.shape({
+      customerName: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired,
+      syncedFrom: PropTypes.string,
+      startDate: PropTypes.string,
+      mrr: PropTypes.number,
+      termLength: PropTypes.number,
+      invoiceNo: PropTypes.number,
+    }).isRequired
+  ).isRequired,
+  columnSettings: PropTypes.shape(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isFilterable: PropTypes.bool,
+      filterAmount: PropTypes.number,
+      shouldDisplay: PropTypes.bool.isRequired,
+    }).isRequired
+  ).isRequired,
+  onColumnToggle: PropTypes.func.isRequired,
+  onApplyFilter: PropTypes.func.isRequired,
 };
 
 export default TableDashboard;
