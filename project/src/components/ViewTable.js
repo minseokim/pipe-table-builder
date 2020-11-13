d6c8a5c7f15bf4713ee74a8c524a4ba37ea81670
minseokim <minseoalexkim@gmail.com>
2020-11-13 10:33:15 +0900

    Apply filter settings to viewTable

--- project/src/components/ViewTable.js
+++ project/src/components/ViewTable.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import { Pane, Table } from 'evergreen-ui';
 import PropTypes from 'prop-types';
+import {
+  NO_OPERATOR_SELECTED,
+  GREATER_THAN,
+  LESS_THAN,
+  EQUAL_TO,
+  NOT_EQUAL_TO,
+} from './FilterPopover';
 
 const ViewTable = ({ columnSettings, customerDataList }) => {
-  const selectedColumnList = Object.keys(columnSettings).filter(
+  const columnNameList = Object.keys(columnSettings);
+  const selectedColumnList = columnNameList.filter(
     (columnName) => columnSettings[columnName].shouldDisplay
   );
 
-  // TODO : Apply filter logic here to every data row(customerData)
-  const applyFilters = () => true;
+  const filterList = selectedColumnList.filter(
+    (columnName) => columnSettings[columnName].filterOperator
+  );
+
+  const applyFilters = (customerData) =>
+    filterList.every((filterName) => {
+      const rowValue = customerData[filterName];
+      const { filterOperator, filterAmount } = columnSettings[filterName];
+
+      switch (filterOperator) {
+        case NO_OPERATOR_SELECTED:
+          return true;
+        case GREATER_THAN:
+          return rowValue > filterAmount;
+        case LESS_THAN:
+          return rowValue < filterAmount;
+        case EQUAL_TO:
+          return rowValue === filterAmount;
+        case NOT_EQUAL_TO:
+          return rowValue !== filterAmount;
+        default:
+          return true;
+      }
+    });
 
   return (
     <Pane flex={3} background="tint1">
